78cbece27009c55254083a2eca23c67ec97fa47a
Ben Lee <1019229581@qq.com>
2018-11-03 12:20:57 +0800

    start

---
+++ memo.md
@@ -0,0 +1,12 @@
+## 6.2
++ .babelrc babel-node   babel-preset-es2015(babel-preset-env)
++ sass-loader  node-sass
+
+## 7.1
+#### 模板设计
+#### 组件设计 
++ 城市服务组件 使用vuex + SSR 一次请求即可获取city数据
++ 用户数据&状态
+
+## 7.2
++  error ReferenceError: document is not defined    关闭cache
---
+++ nuxt.config.js
@@ -0,0 +1,92 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/reset.css',
+    'element-ui/lib/theme-chalk/index.css',
+    '~assets/css/main.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+
+    cache: true,   //加快编译速度
+    extend(config, { isServer, isDev, isClient }) {
+      // Run ESLint on save
+      /*if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }*/
+
+      //https://github.com/nuxt/nuxt.js/issues/3804
+      if (isServer) {
+        for (const rules of config.module.rules.filter(({ test }) =>
+          /\.((c|le|sa|sc)ss|styl.*)/.test(test.toString())
+        )) {
+          for (const rule of rules.oneOf || []) {
+            rule.use = rule.use.filter(
+              ({ loader }) => loader !== 'cache-loader'
+            )
+          }
+        }
+      }
+
+    }
+    
+  }
+}
